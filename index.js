b5076397eb7bd7f86bfc4619af17b12f9531fd3e
Andrey <ashesto121@gmail.com>
2023-01-21 21:59:38 -0800

    added responsiness

--- index.js
+++ index.js
@@ -7,15 +7,12 @@ let initialTime = 60;
 let correctAnswered = 0;
 let answeredQuestions = 0;
 
-let currentChoice;
-
-let questionsLabels = document.querySelector("question-label");
 let answer1 = document.querySelector(".op-1");
 let answer2 = document.querySelector(".op-2");
 let answer3 = document.querySelector(".op-3");
 let answer4 = document.querySelector(".op-4");
 
-let upcoomingQuestion = document.querySelector(".upcoming-question");
+let upcomingQuestion = document.querySelector("#upcoming-question");
 let questionOpt = document.querySelector(".ques-options");
 let correctRes = document.querySelector(".res-2");
 let incorrectRes = document.querySelector(".res-3");
@@ -55,24 +52,75 @@ let myQuestions = [
 	},
 ];
 timer.textContent = 0;
-startTime = () => {
-	countDown = initialTime;
+let countDown = initialTime;
+const startTime = () => {
 	let timerEl = setInterval(function () {
 		countDown--;
 		timer.textContent = countDown;
 		if (countDown <= 0) {
-			clearInterval(timer);
+			clearInterval(timerEl);
 			timer.textContent = 0;
 		} else if (answeredQuestions > 5) {
-			clearInterval(timeEl);
+			clearInterval(timerEl);
 			timer.textContent = 0;
 		}
 	}, 1000);
 };
 
-startQuiz = (event) => {
+const mainCon = () => {
+	document.getElementById("main-content").style.display = "none";
+	document.getElementById("question-content").style.display = "flex";
+};
+
+const displayQuestion = ({ question, answers, correctAnswer }) => {
+	const titleElement = document.getElementById("question-title");
+	titleElement.innerText = question;
+
+	for (const [index, answer] of answers.entries()) {
+		let ogAnswerButton = document.querySelector(`#answer-${index + 1}`);
+		let answerButton = ogAnswerButton.cloneNode(true);
+		ogAnswerButton.parentNode.replaceChild(answerButton, ogAnswerButton);
+		answerButton.innerText = answer;
+
+		answerButton.addEventListener("click", (event) => {
+			event.preventDefault();
+			const answerResponse = document.getElementById("response");
+			answerResponse.innerText =
+				correctAnswer === answer ? "Correct!" : "Incorrect!";
+			if (correctAnswer !== answer) {
+				countDown -= 10;
+			}
+			getNewQuestion();
+		});
+	}
+};
+
+const startQuiz = (event) => {
 	event.preventDefault();
+	mainCon();
+
 	startTime();
+	getNewQuestion();
+};
+const getNewQuestion = () => {
+	console.log(myQuestions);
+	const question = randomQuestion();
+	console.log(myQuestions.length);
+	if (question) {
+		displayQuestion(question);
+	} else {
+		document.getElementById("question-content").style.display = "none";
+		
+	}
+};
+
+const randomQuestion = () => {
+	const selectedQuestion =
+		myQuestions[Math.floor(Math.random() * myQuestions.length)];
+	myQuestions = myQuestions.filter((question) => {
+		return selectedQuestion.question !== question.question;
+	});
+	return selectedQuestion;
 };
 
 startGame.addEventListener("click", startQuiz);
